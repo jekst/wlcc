841fb935079b8bc44a75a470f453e58c3af68ebd
jekst <jekst@qq.com>
2016-07-06 18:58:10 +0800

    init

---
+++ resource/js/wlcc.js
@@ -0,0 +1,23 @@
+$(function() {
+		var swiper = new Swiper('.swiper-container', {
+			autoplay : 3000,
+			autoHeight:true,
+			pagination: '.swiper-pagination',
+			nextButton: '.swiper-button-next',
+			prevButton: '.swiper-button-prev',
+			slidesPerView: 1,
+			paginationClickable: true,
+			spaceBetween: 0,
+			autoplayDisableOnInteraction:false,
+			loop: true
+		});
+		$('.nav a').click(function(){
+			$(this).addClass('on').siblings().removeClass('on');
+			if($(this).hasClass('j-case')){
+				$(window).scrollTop($('#case').position().top-40);
+			}
+		});
+		if(location.hash=="#case"){
+			$('.j-case').click();
+		}
+    });
